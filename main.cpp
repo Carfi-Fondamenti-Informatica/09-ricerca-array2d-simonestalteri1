d222ed32f543330ea42d4128201c34540c6b0b9e
simonestalteri1 <117160924+simonestalteri1@users.noreply.github.com>
2022-12-16 16:45:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
+using namespace std;
+
+int main()
+{
+    
+    char lista[10][20];
+    char nome[20];
+    riempi(lista);
+    riempi2(nome);
+    conf(lista, nome);
+    analisi(lista, nome);
+    
   return 0;
 }
