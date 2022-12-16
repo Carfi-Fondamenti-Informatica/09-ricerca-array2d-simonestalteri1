d160fe780eb7bea6ad3146874a6f3341eb0537b0
simonestalteri1 <117160924+simonestalteri1@users.noreply.github.com>
2022-12-16 16:46:08 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,7 @@
-
+#ifndef _GITHUB_LIB_H
+#define _GITHUB_LIB_H
+void riempi(char [10][20]);
+void riempi2(char[20]);
+bool conf(char[10][20], char [10]);
+void analisi(char a[10][20], char b[20]);
+#endif //_GITHUB_LIB_H
